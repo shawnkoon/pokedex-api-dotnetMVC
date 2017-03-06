5b2fb2b63cd53c1f46c60668aaeb5efebd616772
shawnkoon <shawn9405@gmail.com>
2017-03-06 12:32:42 -0800

    Added rest of CRUD operations & everything works correctly

--- PokedexApi/Controllers/PokedexController.cs
+++ PokedexApi/Controllers/PokedexController.cs
@@ -20,15 +20,63 @@ namespace PokedexApi.Controllers
             return Pokedex.GetAll();
         }
 
-        [HttpGet("{id}", Name ="Do I need?")]
+        [HttpGet("{id}", Name = "GetById")]
         public IActionResult GetById(long id)
         {
             var item = Pokedex.Find(id);
-            if(item == null)
+            if (item == null)
             {
                 return NotFound();
             }
             return new ObjectResult(item);
         }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Pokemon pokemon)
+        {
+            if (pokemon == null)
+            {
+                return BadRequest();
+            }
+
+            Pokedex.Add(pokemon);
+            return CreatedAtRoute("GetById", new { id = pokemon.Id }, pokemon);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] Pokemon pokemon)
+        {
+            if(pokemon == null || pokemon.Id != id)
+            {
+                return BadRequest();
+            }
+
+            Pokemon oldPokemon = Pokedex.Find(id);
+            
+            if(oldPokemon == null)
+            {
+                return NotFound();
+            }
+
+            oldPokemon.IsCaught = pokemon.IsCaught;
+            oldPokemon.Name = pokemon.Name;
+
+            Pokedex.Update(oldPokemon);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var oldPokemon = Pokedex.Find(id);
+            if(oldPokemon == null)
+            {
+                return NotFound();
+            }
+
+            Pokedex.Remove(id);
+            return new NoContentResult();
+        }
+
     }
 }
